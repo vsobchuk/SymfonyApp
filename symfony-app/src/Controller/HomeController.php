439c34f2f4e90c287f5bc168bffba3d2414c55f5
vsobchuk <vmsobchuk@gmail.com>
2026-04-24 13:08:48 +0200

    - upgrade symfony to 7.4

--- symfony-app/src/Controller/HomeController.php
+++ symfony-app/src/Controller/HomeController.php
@@ -17,10 +17,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     * @return JsonResponse
-     */
+    #[Route("/", name: 'home')]
     public function index(Request $request, EntityManagerInterface $em, ManagerRegistry $managerRegistry): Response
     {
         $photoRepository = new PhotoRepository($managerRegistry);
